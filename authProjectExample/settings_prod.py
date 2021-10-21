bf6e500c5536b48932bc7030cc70a1cdb54e2c94
JARNIEL <perezjarda@gmail.com>
2021-10-21 13:34:51 -0500

    chages for deployment

--- authProjectExample/settings_prod.py
+++ authProjectExample/settings_prod.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,4 +153,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
 django_heroku.settings(locals())
