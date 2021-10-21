978137641a3ba26010d73281959a18474cd81b32
jadape <92096697+jadape@users.noreply.github.com>
2021-10-20 22:33:50 -0500

    Delete brand.py

--- authAppExample/models/brand.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.db.models.base import Model
-
-class Brand(models.Model):
-    id   = models.AutoField(primary_key=True)
-    name = models.CharField('brandname', max_length=15, unique=True)
